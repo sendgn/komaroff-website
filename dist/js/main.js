0a5f673958c57bc95f06e86ee23e60f19ec78cfc
polozhenskii <polozhenskii@gmail.com>
2019-09-27 18:53:30 +0300

    photo gallery fixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,15 +2,15 @@
 window.addEventListener("scroll", function() {
   if (window.scrollY > 50) {
     this.document.querySelector("#navbar").style.background =
-      "rgba(0, 0, 0, 0.9)";
-    this.document.querySelector(".navbar-content").style.height = "4rem";
-    this.document.querySelector(".logo h2").style.fontSize = "1.2rem";
-    this.document.querySelector(".logo img").style.height = "40px";
+      "rgba(0, 0, 0, 0.7)";
+    // this.document.querySelector(".navbar-content").style.height = "4rem";
+    // this.document.querySelector(".logo h2").style.fontSize = "1.2rem";
+    // this.document.querySelector(".logo img").style.height = "40px";
   } else {
     this.document.querySelector("#navbar").style.background =
       "rgba(0, 0, 0, 0.5)";
-    this.document.querySelector(".navbar-content").style.height = "5rem";
-    this.document.querySelector(".logo h2").style.fontSize = "1.4rem";
-    this.document.querySelector(".logo img").style.height = "45px";
+    // this.document.querySelector(".navbar-content").style.height = "5rem";
+    // this.document.querySelector(".logo h2").style.fontSize = "1.4rem";
+    // this.document.querySelector(".logo img").style.height = "45px";
   }
 });
